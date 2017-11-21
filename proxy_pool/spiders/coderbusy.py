943e7a83f5a180955d4a73ea10a0335ca79405f1
linjun.yang <linjun.yang@zymobi.com>
2017-11-21 15:31:53 +0800

    换4space

--- proxy_pool/spiders/coderbusy.py
+++ proxy_pool/spiders/coderbusy.py
@@ -5,19 +5,19 @@ from proxy_pool.items import ProxyPoolItem
 
 
 class CoderbusySpider(scrapy.Spider):
-	name = 'coderbusy'
-	allowed_domains = ['coderbusy.com']
-	start_urls = ['https://proxy.coderbusy.com/']
+    name = 'coderbusy'
+    allowed_domains = ['coderbusy.com']
+    start_urls = ['https://proxy.coderbusy.com/']
 
-	def parse(self, response):
-		r = re.compile(r'<tr>[\s\S]*?<\/tr>', re.I|re.M)
-		d = r.findall(s)
-		for each in d[1:]:
-			yield ProxyPoolItem({
+    def parse(self, response):
+        r = re.compile(r'<tr>[\s\S]*?<\/tr>', re.I|re.M)
+        d = r.findall(s)
+        for each in d[1:]:
+            yield ProxyPoolItem({
 				'ip': re.findall(r'\d+\.\d+\.\d+\.\d+', each)[0],
 				'port': re.findall(r'<td>(\d+)<\/td>', each)[0],
 				'types': re.findall(r'<td>([透明匿名高]+)<\/td>', each)[0],
 				'protocol': re.findall(r'<td>([HTTPS]+)<\/td>', each)[0],
 				'address': re.findall(r'\.aspx">([^<]+)<\/a>', each)[0],
 				'website': 'https://proxy.coderbusy.com/'
-				})
+			})
